e07a0bb77fbc321a3669cc4bd1f1264cb008a377
NotArobase <maximegrenet@gmail.com>
2025-01-14 14:17:13 +0100

    environment back-end

---
+++ server/utils/env.js
@@ -0,0 +1,13 @@
+const DB_DB = "TP5";
+const DB_COLLECTION_SONGS = "songs";
+const DB_COLLECTION_PLAYLISTS = "playlists";
+const DB_COLLECTION_USERS = "users";
+
+// TODO : fournir les bonnes informations de connexion (les <> doivent être enlevés)
+const DB_URL = "mongodb+srv://arobase:admin@cluster0.papma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+"
+exports.DB_DB = DB_DB;
+exports.DB_COLLECTION_SONGS = DB_COLLECTION_SONGS;
+exports.DB_COLLECTION_PLAYLISTS = DB_COLLECTION_PLAYLISTS;
+exports.DB_COLLECTION_USERS = this.DB_COLLECTION_USERS
+exports.DB_URL = DB_URL;
