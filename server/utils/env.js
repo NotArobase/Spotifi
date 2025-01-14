87add1ffac026f72a2b6ac3781ab70f9dd6f6204
NotArobase <maximegrenet@gmail.com>
2025-01-14 14:17:28 +0100

    fix

--- server/utils/env.js
+++ server/utils/env.js
@@ -4,8 +4,9 @@ const DB_COLLECTION_PLAYLISTS = "playlists";
 const DB_COLLECTION_USERS = "users";
 
 // TODO : fournir les bonnes informations de connexion (les <> doivent être enlevés)
-const DB_URL = "mongodb+srv://arobase:admin@cluster0.papma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-"
+const DB_URL = "mongodb+srv://arobase:admin@cluster0.papma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
 exports.DB_DB = DB_DB;
 exports.DB_COLLECTION_SONGS = DB_COLLECTION_SONGS;
 exports.DB_COLLECTION_PLAYLISTS = DB_COLLECTION_PLAYLISTS;
