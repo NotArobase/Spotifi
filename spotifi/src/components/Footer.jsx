ed250bb826a47a7bf3a73dd4c3768aba4339860f
NotArobase <maximegrenet@gmail.com>
2025-01-21 10:05:51 +0100

    fix footer + fix playlists/songs empty

--- spotifi/src/components/Footer.jsx
+++ spotifi/src/components/Footer.jsx
@@ -1,28 +1,10 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Player from "./Player";
 
 export default function Footer() {
   function loadPlayer(pathname) {
-    if (pathname.startsWith("/playlist")) {
-      return <Player />;
-    } else {
-      return (
-        <div>
-          <p>
-            Choisir une playlist à travers la
-            <NavLink
-              to="/index"
-              className="text-blue-500 hover:text-blue-700 underline transition-colors duration-200"
-            >
-              {" "}
-              Bibliothèque
-            </NavLink>{" "}
-            pour la faire jouer
-          </p>
-        </div>
-      );
-    }
+    return <Player />;
   }
 
   return (
